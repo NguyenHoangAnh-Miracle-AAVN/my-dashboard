78ee453170cffc8e0f1bc2389fcc06286858c395
[Miracle] Nguyen Hoang Anh <anh.nguyenhoang01@axonactive.com>
2024-05-23 15:18:16 +0700

    implement the metadata for SEO friendly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,22 @@
 import '@/app/ui/global.css';
 import {inter, lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
-
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router',
+  metadataBase: new URL('https://my-dashboard-nextjs-five.vercel.app')
+}
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
